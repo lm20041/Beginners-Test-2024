bba521bac3db2edb7125c4c36969c2f0e8e4288a
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-02-12 21:21:09 +0000

    input & output -text1

---
+++ main.py
@@ -0,0 +1,13 @@
+# ask the user to enter there name
+username = input("Enter your username: ")
+
+# ask the user to enter their favourite number (int)
+fav_num = int(input("Enter your favourite number: "))
+
+# double, halve & square the number
+double_num = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+#greet the user
+print("Hello", username, "your favourite number is", fav_num, "to doubled it is", double_num, "to halved it is", halve, "and to squared it is", square)
