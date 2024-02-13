4b09c2372e45d4db23485041a8337ff742af837e
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-02-13 21:11:50 +0000

    05 number checker-test1

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 # ask the user for width and loop until they
 # enter a number that is more than zero
-error = "Invalid, please enter a valid"
-while True:
+def num_check(question):
 
-    try:
-        #ask the user for width
-        width = float(int(input("Enter the width: ")))
-        #check that the number is more than zero
-        if width > 0:
-          break
-        else:
-          print(error, "input")
-    except ValueError:
-        print(error, "input")
+    error = "Invalid, please enter a valid"
+    while True:
+    
+        try:
+            #ask the user for width
+            response = float(input("Enter the " + question + ": "))
+            #check that the number is more than zero
+            if response > 0:
+              return response
+            else:
+              print(error, "input")
+        except ValueError:
+            print(error, "input")
+
+# main routine goes here
+for item in range(0, 2):
+    width = num_check("Width")
+    print(width)
+
+for item in range(0, 2):
+    height = num_check("Height")
+    print(height)
