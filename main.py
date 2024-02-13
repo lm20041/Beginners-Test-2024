f111b2be849bf783ea6bf1112f96505a456b3320
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-02-13 21:51:15 +0000

    06 area perimeter complete-Test1

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-# ask the user for width and loop until they
-# enter a number that is more than zero
-def num_check(question):
+#Ask the user for the width and height
+# (assume they put  in vaild data)
+def calculate_area_perimeter():
+  global width
+  global height
+  while True:
+    try:
+      width = float(input("Enter the width: "))
+      height = float(input("Enter the height: "))
+      if width > 0 and height > 0:
+        return
+      else:
+        print("Please enter a valid number")
+    except ValueError:
+      print("Please enter a valid number")
+calculate_area_perimeter()
+#calculate the area / perimeter
+area = width * height
+perimeter = 2 * (width + height)
+#output the area and perimeter
+print("The area is", width,"in width" , height,"in height")
+print("The perimeter is", perimeter)
 
-    error = "Invalid, please enter a valid"
-    while True:
-    
-        try:
-            #ask the user for width
-            response = float(input("Enter the " + question + ": "))
-            #check that the number is more than zero
-            if response > 0:
-              return response
-            else:
-              print(error, "input")
-        except ValueError:
-            print(error, "input")
-
-# main routine goes here
-for item in range(0, 2):
-    width = num_check("Width")
-    print(width)
-
-for item in range(0, 2):
-    height = num_check("Height")
-    print(height)
+#-------------------------------------#
