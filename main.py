faa15f8296e3fe439e5946dca4c899e9e86fe7a8
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-02-23 00:24:38 +0000

    02VariablesPart 1-Test1

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-word = input('Enter a word that rhymes with "free": ')
-print('SNow for a short poem.')
-print(word)
-print('hi')
+#code to learn about variables...
+
+#variables can be a string (note the speech marks)
+greeting = "hello world"
+
+#variables can be a number
+fav_num = 7
+#num_string
+num_string = "5"
+#variables can be a boolean (true or false)
+love_python = True
+
+#printing variables
+print(greeting)
+print(fav_num)
+print(num_string)
+print(love_python)
+#these variable has a problem
+# my name = "Miss G"
+# 1item = "apple"
+#variable Cannot be two separate Entities and must start with a letter not a number
+
+#print variable in name will come out as the name not it's value
+print('num_string')
+
+#num_string: int & string is two different things
+#int it's value times the number 5 = (35) 
+print(fav_num * 5)
+#where for string it just prints itself 5 times (55555)
+print(num_string * 5)
