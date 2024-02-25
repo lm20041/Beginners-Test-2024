8db9abd63d584534baca802e3323aea11eda0e26
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-02-25 02:38:19 +0000

    10 filetype checker_V1-test2

--- main.py
+++ main.py
@@ -18,9 +18,35 @@ Instructions go here.
   ''')
   
   statement_generator_end("END","-")
+#
+def get_filetype():
+  response = input("File type: ").lower()
+
+  # check for 'i' or the exit code
+  if response == "i" or response == "xxx":
+    return response
+  
+  # check if it's an integer
+  elif response in ['integer', 'int']:
+    return "integer"
+  # check for an image
+  elif response in ['image', 'picture', 'img']:
+    return "image"
+  # check for an text
+  elif response in ['text', 'txt']:
+    return "text"
+
+  # if the response is invalid output an error
+  else:
+    print("Please enter a valid file type")
 
 # Main routine goes here
+while True:
+  file_type = get_filetype()
+  print(f"you chose {file_type}")
 
+  if file_type == "xxx":
+    break
 # Display instructions if requested
 want_instructions = input(f"Press <enter> to read the instructions or any key to continue: ").lower()
 
