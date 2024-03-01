dde052cf2a45980f2b41cf51eb4dbb9c46b39e57
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-01 23:11:19 +0000

    12 text image calcs_V1-test2

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+def int_check(question, low):
+  error = "Please enter a number that is more than 0\n"
+  while True:
+    try:
+      #ask the uesr for a number
+      response = int(input(question))
+      # check that the number is more than zero
+      if response > low:
+        return response
+      else:
+        print(error)
+    except ValueError:
+      print(error)
+
+#Calculates how many bits are needed to represent an integer
+def image_calc():
+  # get the image dimensions
+  width = int_check("Width: ", 0)
+  height = int_check("Height: ", 0)
+
+  # calculate the number of pixels and multiply by 24 to get the number of bits
+  num_pixels = width * height
+  num_bits = num_pixels * 24
+
+  # set up answer and return it
+  answer = (f"Number of pixels: {width} X {height} = {num_pixels}" 
+    f"\nNumber of bits {num_pixels} X 24 ={num_bits}")
+  return answer
 #Calculates number of bits needed to represent text in ascii
 def calc_text_bits():
   #get text from user
@@ -15,3 +43,6 @@ def calc_text_bits():
 #Main Routine goes here
 text_ans = calc_text_bits()
 print(text_ans)
+print()
+image_ans = image_calc()
+print(image_ans)
