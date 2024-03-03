469df01fc85c62048bfdd73d8076a8ecd9e7e545
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-03 01:46:01 +0000

    19 conversion hints_V1-test2

--- main.py
+++ main.py
@@ -8,16 +8,32 @@ my_dict = {
   "black": "night",
   "white": "day",
 }
-
 to_find = input("Enter a colour: ")
 # check if it's a key...
 if to_find in my_dict:
   # get the value...
   coloured_object = my_dict[to_find]
-  
   print(f"The colour {to_find} is {coloured_object}")
-
 elif to_find in my_dict.values():
   print(f"{to_find} is a value in the dicionary")
 else:
   print("Colour not found")
+
+distance_dict = {
+  "mm": 1000,
+  "cm": 100,
+  "m": 1,
+  "km": 0.001,
+}
+
+# get amount and units (assume they are valid)
+amount = float(input("how much? "))
+from_unit = input("from unit? ")
+to_unit = input("to unit? ")
+# Multiply to get to our standard value...
+multiply_by = distance_dict[from_unit]
+standard = amount * multiply_by
+# divide to get to our desired value...
+divide_by = distance_dict[from_unit]
+answer = standard / divide_by
+print(f"{amount} {from_unit} is {answer} {to_unit}")
